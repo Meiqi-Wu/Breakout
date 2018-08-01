c4a4369cff5ce7662a43eb66efc8caf5cd9d2f0b
Meiqi-Wu <meiqiwu9878@gmail.com>
2018-07-31 23:40:39 -0400

    test of GLabel

--- src/mousereporter/MouseReporter.java
+++ src/mousereporter/MouseReporter.java
@@ -1,5 +1,21 @@
 package mousereporter;
 
-public class MouseReporter {
+import javax.swing.JFrame;
 
+public class MouseReporter extends JFrame{
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("test frame");
+		frame.setSize(300, 400);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		GLabel label = new GLabel("test label hahahahahhahahah");
+		label.setLocation(150, 150);
+		frame.getContentPane().add(label);
+		frame.revalidate();
+		frame.repaint();
+		System.out.println("width: "+label.getWidth());
+		System.out.println("height: "+label.getHeight());
+	}
+	
 }
